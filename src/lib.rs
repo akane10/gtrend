3fa591790e5ac5028f83282b643f1f69c79647c8
yapie <targaryen.akane@gmail.com>
2020-10-09 23:43:54 +0700

    some more test

--- src/lib.rs
+++ src/lib.rs
@@ -64,4 +64,56 @@ mod tests {
 
         assert!(data.is_ok())
     }
+
+    #[test]
+    fn get_developers_username_should_always_some() {
+        let data = developers::get_data(None, SINCE).unwrap();
+
+        let y: Vec<_> = data
+            .clone()
+            .into_iter()
+            .filter(|x| x.username.is_none())
+            .collect();
+
+        assert!(y.is_empty())
+    }
+
+    #[test]
+    fn get_developers_name_should_always_some() {
+        let data = developers::get_data(None, SINCE).unwrap();
+
+        let y: Vec<_> = data
+            .clone()
+            .into_iter()
+            .filter(|x| x.name.is_none())
+            .collect();
+
+        assert!(y.is_empty())
+    }
+
+    #[test]
+    fn get_developers_url_should_always_some() {
+        let data = developers::get_data(None, SINCE).unwrap();
+
+        let y: Vec<_> = data
+            .clone()
+            .into_iter()
+            .filter(|x| x.url.is_none())
+            .collect();
+
+        assert!(y.is_empty())
+    }
+
+    #[test]
+    fn get_developers_avatar_should_always_some() {
+        let data = developers::get_data(None, SINCE).unwrap();
+
+        let y: Vec<_> = data
+            .clone()
+            .into_iter()
+            .filter(|x| x.avatar.is_none())
+            .collect();
+
+        assert!(y.is_empty())
+    }
 }
