dfe76702f5230d17208849976b32abf8c7e511fb
yapie <targaryen.akane@gmail.com>
2021-03-15 17:00:24 +0700

    commit

--- src/languages.rs
+++ src/languages.rs
@@ -4,14 +4,11 @@ use serde_json::Value;
 pub fn get_data() -> Vec<Language> {
     let bytes = include_bytes!("../languages.json");
     let data = Language::get_data(bytes);
-
-    // println!("language: {:?}", data[0]);
     data
 }
 
 pub fn get_data_json() -> Value {
     let data = get_data();
-
     let data_json: Value = Language::get_data_json(data);
     data_json
 }
